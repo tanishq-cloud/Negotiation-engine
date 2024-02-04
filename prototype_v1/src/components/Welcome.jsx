0408e06f08f58ab88b48bb68ead9b8b527c9495b
tanishq-cloud <117560477+tanishq-cloud@users.noreply.github.com>
2024-02-05 00:23:47 +0530

    Added prototype version 1

---
+++ prototype_v1/src/components/Welcome.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const Welcome = () => {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  return (
+    <main className="welcome">
+      <h2>Welcome to Negotiation Portal.</h2>
+      <p>Sign in with Google to access the portal.</p>
+      <button className="sign-in">
+        <img
+          onClick={googleSignIn}
+          src={GoogleSignin}
+          alt="sign in with google"
+          type="button"
+        />
+      </button>
+    </main>
+  );
+};
+
+export default Welcome;
