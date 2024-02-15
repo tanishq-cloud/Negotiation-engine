8dd19ef55cd13d953ee459e3ace144051f93adfd
Tanishq Kumar <117560477+tanishq-cloud@users.noreply.github.com>
2024-02-15 16:49:49 +0530

    Update firebase.js

--- prototype_v1/src/firebase.js
+++ prototype_v1/src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWbF4gsy6eBJUGdWgAVbAwOXv3_-A2rTI",
-  authDomain: "negotiation-engine.firebaseapp.com",
-  projectId: "negotiation-engine",
-  storageBucket: "negotiation-engine.appspot.com",
-  messagingSenderId: "732649527029",
-  appId: "1:732649527029:web:f0a28d22c9e43ac4a917bc",
-  measurementId: "G-3QEHR01RYX"
+  //config
 };
 
 const app = initializeApp(firebaseConfig);
